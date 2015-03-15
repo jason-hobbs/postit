956a351ab7102f17808015c3f96e8d4d3e0594da
Jason Hobbs <grimmstede@feemon.com>
2015-03-15 17:51:42 -0500

    added postcategory link table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309202044) do
+ActiveRecord::Schema.define(version: 20150315041122) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20150309202044) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "url"
