b8f538c70c6ff3be8b8adff604efdca1b3661a01
Jason Hobbs <grimmstede@feemon.com>
2015-03-29 21:52:12 -0500

    finished nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 def create
   @post = Post.find(params[:post_id])
   @comment = @post.comments.build(params.require(:comment).permit(:body))
+  @comment.user_id = User.first.id
 
   if @comment.save
     flash[:notice] = "Comment created"
