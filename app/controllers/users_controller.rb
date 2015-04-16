baff9568aed974c3d1aa4bfc0f32beb9df02a690
Jason Hobbs <grimmstede@feemon.com>
2015-04-16 15:29:39 -0500

    only allow editing self on profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :get_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -41,6 +42,13 @@ class UsersController < ApplicationController
 
   def get_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You are not allowed to do that"
+      redirect_to root_path
+    end
   end
 
 end
