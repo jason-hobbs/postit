d06f4ad88ad28c8ef53c3f0c4ddfd0c324c5a9ec
Jason Hobbs <grimmstede@feemon.com>
2015-05-07 15:10:19 -0500

    added slugs for user and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,14 @@ class Category < ActiveRecord::Base;
   has_many :post_categories
   has_many :posts, through: :post_categories
   validates :name, presence: true, length: {minimum: 5}
+
+  before_save :generate_slug
+
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.name.gsub(" ", "-").downcase
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base;
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
+
+  before_save :generate_slug
+
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.username.gsub(" ", "-").downcase
+  end
 end
