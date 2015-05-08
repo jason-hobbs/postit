f8189acf3c6b246a48a55ff52d0900a2056a749d
Jason Hobbs <grimmstede@feemon.com>
2015-05-08 14:36:04 -0500

    moved total_votes to module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,6 @@
 class Comment < ActiveRecord::Base;
+  include Voteable
   belongs_to :creator , foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
-  has_many :votes, as: :voteable
   validates :body, presence: true, length: {minimum: 5}
-
-  def total_votes
-    self.votes.where(vote: true).size - self.votes.where(vote: false).size
-  end
 end
---
+++ lib/voteable.rb
@@ -0,0 +1,21 @@
+module Voteable
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.extend ClassMethods
+    base.class_eval do
+      assoc
+    end
+  end
+
+  module InstanceMethods
+    def total_votes
+      self.votes.where(vote: true).size - self.votes.where(vote: false).size
+    end
+  end
+
+  module ClassMethods
+    def assoc
+      has_many :votes, as: :voteable
+    end
+  end
+end
