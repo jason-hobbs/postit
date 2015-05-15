c6d0a85b9c321b23272fd4b887c811b2c2c91eb8
Jason Hobbs <grimmstede@feemon.com>
2015-05-15 13:55:46 -0500

    added roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505021141) do
+ActiveRecord::Schema.define(version: 20150515183952) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150505021141) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "slug"
+    t.string   "role"
   end
 
   create_table "votes", force: :cascade do |t|
