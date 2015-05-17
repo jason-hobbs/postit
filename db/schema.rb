3c4edd5e5b0c0038d00a7b93f59db7a9b20904ed
Jason Hobbs <grimmstede@feemon.com>
2015-05-17 08:16:40 -0500

    added timezone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515183952) do
+ActiveRecord::Schema.define(version: 20150517022735) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150515183952) do
     t.string   "password_digest"
     t.string   "slug"
     t.string   "role"
+    t.string   "time_zone"
   end
 
   create_table "votes", force: :cascade do |t|
